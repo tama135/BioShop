896a5fe7b18d9e2c159a15d9445e0cac8f25adfa
tama135 <48179858+tama135@users.noreply.github.com>
2022-09-15 15:29:21 +0200

    Delete 9. razni neki projekti directory

--- 9. razni neki projekti/CandyStore/candy_store_backend/src/main/java/rs/ac/singidunum/candy_store_backend/repository/IUsersRepository.java
+++
@@ -1,9 +0,0 @@
-package rs.ac.singidunum.candy_store_backend.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import rs.ac.singidunum.candy_store_backend.entity.Users;
-
-public interface IUsersRepository extends MongoRepository<Users, String> {
-    Users findByUsername(String username);
-    Users findByEmail(String email);
-}
